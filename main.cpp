07529c46e9b0c68f0722d189c163047b294b9919
ahmedr6 <ahmedhaza14@gmail.com>
2023-10-10 13:06:45 +0300

    add cpp

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+        int sum=0;
+    float avg;
+     int arr[1000];
+    int x;
+    printf("plz enter the size of the array \n");
+    scanf("%d",&x);
+    for(int i=0;i<x;i++)
+    {
+        printf("plz enter the score of the subject number %d\n",i+1);
+        scanf("%d",&arr[i]);
+
+       sum+=arr[i];
+
+    }
+    avg=(float)sum/x;
+    printf("%f",avg);
+
+    return 0;
+}
